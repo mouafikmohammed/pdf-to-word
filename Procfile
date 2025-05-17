63a2a489b6d6f0ab979c4b3959809ebc7b548848
mouafikmohammed <mouafikmohammed03@gmail.com>
2025-05-17 18:04:52 +0100

    add

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pdftoword.wsgi:application
--- pdftoword/settings.py
+++ pdftoword/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-u&!5kr5)h%h#-rg4c1)kc01lr@!gxm-j^4!ldecp7v8tv^y4ge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'pdf-to-word.onrender.com',
+    '172.0.0.1', 
+]
 
 
 # Application definition
@@ -116,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
